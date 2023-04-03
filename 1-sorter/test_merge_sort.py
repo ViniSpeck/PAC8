65550e2a427d0e370195230fdd6d51b8784c8545
Vini Speck <vinicius.speck@catolicasc.org.br>
2023-04-03 00:26:40 -0300

    Adjusting Dockerfile and docker-compose parameters

--- 1-sorter/test_merge_sort.py
+++ 1-sorter/test_merge_sort.py
@@ -1,4 +1,4 @@
-import pytest
+import pytest, requests
 from merge_sort import MergeSort
 
 @pytest.fixture
@@ -24,3 +24,13 @@ def test_sort_reverse_sorted_list(sorter):
 def test_sort_random_list(sorter):
     arr = [64, -34, 25, 12, -22, 11, 90]
     assert sorter.sort(arr) == [-34, -22, 11, 12, 25, 64, 90]
+
+# def test_sort_endpoint():
+#     unsorted_arr = [3, 1, 4, 2, 5]
+#     payload = {'arr': unsorted_arr}
+
+#     response = requests.post('http://localhost:5000/sort', json=payload)
+#     assert response.status_code == 200
+
+#     expected_result = [1, 2, 3, 4, 5]
+#     assert response.json()['sorted_arr'] == expected_result
